a89245e79e7eb2e3fa453267cc7cde6a40b49779
Maxim Vanyushkin <maxim.vanyushkin@fastdev.se>
2018-12-15 19:12:11 +0400

    ASDASD

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var physicalFileSystem = new PhysicalFileSystem(@".\webapp\dist");
+            var fileServerOptions = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                RequestPath = PathString.Empty,
+                FileSystem = physicalFileSystem
+            };
+
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames = new[] {"index.html"};
+            fileServerOptions.StaticFileOptions.ServeUnknownFileTypes = true;
+            fileServerOptions.StaticFileOptions.FileSystem = physicalFileSystem;
+            fileServerOptions.StaticFileOptions.RequestPath = PathString.Empty;
+            app.UseFileServer(fileServerOptions);
+            app.UseDirectoryBrowser();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
